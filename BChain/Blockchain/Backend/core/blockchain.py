25a59fe1ab5cc8ea8ff6ca0718507a1117798824
vijayantpawar <31182058+vijayantpawar@users.noreply.github.com>
2023-12-30 21:18:42 +0530

    Add files via upload

---
+++ BChain/Blockchain/Backend/core/blockchain.py
@@ -0,0 +1,45 @@
+import sys
+sys.path.append('/python_basics/BChain')
+
+from Blockchain.Backend.core.block import Block
+from Blockchain.Backend.core.blockheader import BlockHeader
+from Blockchain.Backend.util.util import hash256
+import time
+import json
+
+ZERO_HASH = "0" * 64
+VERSION = 1
+
+class Blockchain:
+    def __init__(self):
+        self.chain =[]
+        self.GenesisBlock()
+
+    def GenesisBlock(self):
+        BlockHeight = 0
+        prevBlockHash = ZERO_HASH
+        self.addBlock(BlockHeight, prevBlockHash)
+
+    def addBlock(self, BlockHeight, prevBlockHash):
+        timestamp = int(time.time())
+        Transaction = f"Codies Alert sent {BlockHeight} Bitcoins to jeo"
+        merkleRoot = hash256(Transaction.encode()).hex()
+        bits = 'ffff001f'
+        blockheader = BlockHeader(VERSION, prevBlockHash, merkleRoot, timestamp, bits) 
+        blockheader.mine()
+        self.chain.append(Block(BlockHeight, 1, blockheader.__dict__, 1, Transaction).__dict__)
+        print(json.dumps(self.chain, indent= 4))
+
+    def main(self):
+        i = 0
+        while i < 3:
+            lastBlock = self.chain[::-1]
+            BlockHeight = lastBlock[0]["Height"] + 1
+            prevBlockHash = lastBlock[0]['BlockHeader']['blockHash']
+            self.addBlock(BlockHeight, prevBlockHash)
+            i += 1
+
+
+if __name__ == "__main__":
+    blockchain = Blockchain()
+    blockchain.main()
